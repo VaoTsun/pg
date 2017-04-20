2312986b4a6e99075bcb3c838c1a0cb2c4a36c10
VaoTsun <vodevsh@gmail.com>
2017-04-20 16:01:07 +0100

    Create biggest.relations.sql

---
+++ biggest.relations.sql
@@ -0,0 +1,21 @@
+--Biggest Relations
+SELECT oid, table_schema, table_name,row_estimate, total_bytes
+    ,pg_size_pretty(total_bytes) AS total
+    , pg_size_pretty(index_bytes) AS INDEX
+    , pg_size_pretty(toast_bytes) AS toast
+    , pg_size_pretty(table_bytes) AS tbl
+  FROM (
+  SELECT *, total_bytes-index_bytes-COALESCE(toast_bytes,0) AS table_bytes FROM (
+      SELECT c.oid,nspname AS table_schema, relname AS TABLE_NAME
+              , c.reltuples AS row_estimate
+              , pg_total_relation_size(c.oid) AS total_bytes
+              , pg_indexes_size(c.oid) AS index_bytes
+              , pg_total_relation_size(reltoastrelid) AS toast_bytes
+          FROM pg_class c
+          LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
+          WHERE relkind = 'r'
+  ) a
+) a
+order by "total_bytes" desc 
+limit 40
+;
